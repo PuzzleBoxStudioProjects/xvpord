094417cffc70bbfe98a6b397eeaf144ff9b49934
Drew <coderage9001@gmail.com>
2015-06-09 14:30:25 -0400

    A quick commit

---
+++ Assets/VRExtreme/Scripts/RedBullRSSFeed.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedBullRSSFeed : MonoBehaviour
+{
+	// Use this for initialization
+	IEnumerator Start ()
+	{
+		WWW www = new WWW("http://www.redbull.com/us/en/rss/index.rss");
+		yield return www;
+	}
+
+	IEnumerator DisplayFeed(string url)
+	{
+		Debug.Log(url);
+
+		WWW www = new WWW(url);
+		yield return www;
+		GetComponent<Renderer>().material.mainTexture = www.texture;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
