35e3be8fd0e84be4db26e2811bf139b47c15ae76
patrick rasmussen <mcman407@gmail.com>
2015-06-10 13:55:39 -0400

    finished main menu and half of rss feed is finished.

---
+++ Assets/VRExtreme/Scripts/Managers/MediaManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MediaManager : MonoBehaviour 
+{
+
+	void Awake () 
+	{
+		XtremeManager.OnUpdate += new XtremeManager.XtremeUpdate(OnUpdate);
+		Buttons.ButtonPressed += new Buttons.ButtonEvent(OnHit);
+	}
+	
+	void OnDestroy()
+	{
+		XtremeManager.OnUpdate -= new XtremeManager.XtremeUpdate(OnUpdate);
+		Buttons.ButtonPressed -= new Buttons.ButtonEvent(OnHit);
+	}
+	// Update is called once per frame
+	void OnUpdate () 
+	{
+		
+	}
+	
+	void OnHit(string name)
+	{
+		switch(name)
+		{
+		case "back":{Application.LoadLevel(0);}break;
+		}
+	}
+}
---
+++ Assets/VRExtreme/Scripts/Tools/Reticle.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reticle : MonoBehaviour 
+{
+	private RaycastHit 	hit;
+	private Transform 	cameraObject;
+	private Vector3 	origScale;
+	private Camera 	  	lookCamera;
+	private float 		distance;	
+
+	// Use this for initialization
+	void Awake () 
+	{
+		XtremeManager.OnUpdate += new XtremeManager.XtremeUpdate(OnUpdate);
+	}
+	void Start()
+	{
+
+		cameraObject = GameObject.Find("RightEyeAnchor").GetComponent<Transform>()as Transform;
+		lookCamera = cameraObject.GetComponent<Camera>();
+		origScale = this.transform.localScale;
+	}
+	void OnDestroy()
+	{
+		XtremeManager.OnUpdate -= new XtremeManager.XtremeUpdate(OnUpdate);
+	}
+	// Update is called once per frame
+	void OnUpdate () 
+	{
+		if(Physics.Raycast(cameraObject.transform.position, cameraObject.transform.rotation * Vector3.forward * 3f, out hit))
+		{
+			if(hit.transform.tag != "ignore")
+			{
+
+				distance = hit.distance * .65f;
+			}
+			else
+			{
+
+			}
+		}
+		else
+		{
+			distance = lookCamera.farClipPlane;
+		}
+
+		transform.position = cameraObject.transform.position + cameraObject.transform.rotation * Vector3.forward * (distance * .55f);
+		transform.LookAt(cameraObject.transform.position);
+		transform.Rotate(0,180f,0);
+
+	}
+}
